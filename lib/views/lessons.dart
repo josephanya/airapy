e8e6df5fb3c2f476db829a85bda1f7cbfe1661d2
Joseph Anya <user@USERs-MacBook-Pro.local>
2020-11-27 01:20:09 +0100

    new commit

--- lib/views/lessons.dart
+++ lib/views/lessons.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class Lessons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
