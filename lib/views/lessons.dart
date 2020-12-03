46cd6f7a80f3ddfccf0e497c08aa1556db312ff9
Joseph Anya <josephanya4real@gmail.com>
2020-12-03 18:40:49 +0100

    new commit

--- lib/views/lessons.dart
+++ lib/views/lessons.dart
@@ -1,9 +1,20 @@
+import 'package:airapy/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:airapy/theme/theme.dart';
 import 'package:airapy/utilities/margin.dart';
+import 'package:airapy/views/lesson_webview.dart';
 import 'package:airapy/widgets/appbar.dart';
-import 'package:flutter/material.dart';
 
-class Lessons extends StatelessWidget {
+class Lessons extends StatefulWidget {
+  static const String id = 'lesson';
+  @override
+  _LessonsState createState() => _LessonsState();
+}
+
+class _LessonsState extends State<Lessons> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,16 +22,122 @@ class Lessons extends StatelessWidget {
       appBar: CustomMainAppBar(
         title: 'Lessons',
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 13),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              YMargin(10),
-            ],
-          ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 13),
+        child: Column(
+          children: <Widget>[
+            YMargin(20),
+            YMargin(20),
+            LessonBody(),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class ResourceBody extends StatelessWidget {
+  const ResourceBody({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class SingleLesson extends StatelessWidget {
+  final String title, url, thumbnail;
+  const SingleLesson({
+    this.title,
+    this.thumbnail,
+    this.url,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 6),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (BuildContext context) => LessonWebview(
+                title: 'Lesson',
+                selectedUrl: url,
+              ),
+            ),
+          );
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              child: Image.network(
+                thumbnail,
+                height: 120,
+              ),
+              width: double.infinity,
+              height: 120,
+              decoration: BoxDecoration(
+                color: background,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+            ),
+            YMargin(12),
+            Center(
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LessonBody extends StatelessWidget {
+  const LessonBody({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var provider = Provider.of<UserModel>(context);
+    return Flexible(
+      child: StreamBuilder(
+        stream: Firestore.instance
+            .collection('users')
+            .document(provider.userID)
+            .collection('lessons')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: Text('Your lessons will appear here'),
+            );
+          }
+          final lessons = snapshot.data.documents;
+          return GridView.builder(
+            itemCount: lessons.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0),
+            itemBuilder: (context, index) {
+              var data = lessons[index];
+              return SingleLesson(
+                title: data['title'],
+                thumbnail: data['thumbnail'],
+                url: data['url'],
+              );
+            },
+          );
+        },
       ),
     );
   }
