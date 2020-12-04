b98dc2d9f82a05d27d867e23f314ad02fbd34995
Joseph Anya <josephanya4real@gmail.com>
2020-12-04 21:49:27 +0100

    new commit

--- lib/views/aerobic_exercise.dart
+++ lib/views/aerobic_exercise.dart
@@ -1,14 +1,56 @@
+import 'package:airapy/theme/theme.dart';
+import 'package:airapy/utilities/margin.dart';
 import 'package:airapy/widgets/appbar.dart';
+import 'package:airapy/widgets/buttons.dart';
+import 'package:airapy/widgets/default_card.dart';
 import 'package:flutter/material.dart';
 
 class AerobicExercise extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: background,
       appBar: CustomSecAppBar(
         title: 'Aerobic exercise',
       ),
-      body: SingleChildScrollView(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 13),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            YMargin(10),
+            DefaultCard(
+              cardChild: Image.asset("images/jumping_jack.gif"),
+            ),
+            YMargin(30),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: Text(
+                'Jumping jacks',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            YMargin(20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: Text(
+                'Start with your feet together and your arms by your sides, then jump up with your feet apart and your hands over your head. Return to the start position and repeat.',
+                style: TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            YMargin(30),
+            PrimaryButton(
+              onPressed: () {},
+              buttonText: 'Start',
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,7 @@
 import 'package:airapy/theme/aircon_icons.dart';
 import 'package:airapy/theme/theme.dart';
 import 'package:airapy/utilities/margin.dart';
+import 'package:airapy/views/aerobic_exercise.dart';
 import 'package:airapy/views/coach_chat.dart';
 import 'package:airapy/views/profile.dart';
 import 'package:airapy/views/track_food.dart';
@@ -115,7 +116,7 @@ class Home extends StatelessWidget {
                 onPress: () => Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => CoachChat(),
+                    builder: (context) => AerobicExercise(),
                   ),
                 ),
               ),
